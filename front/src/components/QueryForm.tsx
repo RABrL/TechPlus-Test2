41d49d4a2414e71758e70549e2107ba9f4747d11
RABrL <robin_brito@hotmail.com>
2023-11-21 15:57:08 -0500

    Add Radix UI Checkbox and Slider components

--- front/src/components/QueryForm.tsx
+++ front/src/components/QueryForm.tsx
@@ -8,7 +8,7 @@ import {
 } from '@/components/ui/form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
-import { z } from 'zod'
+import { optional, z } from 'zod'
 import Button from '@/components/Button'
 import {
   Select,
@@ -17,23 +17,34 @@ import {
   SelectTrigger,
   SelectValue
 } from '@/components/ui/select'
+import { Checkbox } from './ui/checkbox'
+import { Slider } from '@/components/ui/slider'
 
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: 'Username must be at least 2 characters.'
+const formSchema = z
+  .object({
+    Year: z.string(),
+    County_of_Residence: z.string(),
+    Ave_Age_of_Mother: z.array(z.number().min(15).max(100)),
+    Ave_Birth_Weight_gms: z.array(z.number().min(3000).max(3400)),
+    Mothers_Single_Race: z.string(),
+    Abnormal_Conditions_Checked_YN: z.boolean()
   })
-})
+  .partial()
+  .required({ Ave_Age_of_Mother: true, Ave_Birth_Weight_gms: true })
 
 export default function QueryForm() {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      username: ''
+      Abnormal_Conditions_Checked_YN: false,
+      Ave_Age_of_Mother: [15],
+      Ave_Birth_Weight_gms: [3000],
+      County_of_Residence: '',
+      Mothers_Single_Race: '',
+      Year: ''
     }
   })
   function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
     console.log(values)
   }
   return (
@@ -41,10 +52,10 @@ export default function QueryForm() {
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
         <FormField
           control={form.control}
-          name="username"
+          name="Year"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Username</FormLabel>
+              <FormLabel>Año</FormLabel>
               <FormControl>
                 <Select
                   onValueChange={field.onChange}
@@ -52,17 +63,103 @@ export default function QueryForm() {
                 >
                   <FormControl>
                     <SelectTrigger>
-                      <SelectValue placeholder="Select a verified email to display" />
+                      <SelectValue placeholder="Select a year" />
                     </SelectTrigger>
                   </FormControl>
                   <SelectContent>
-                    <SelectItem value="m@example.com">m@example.com</SelectItem>
-                    <SelectItem value="m@google.com">m@google.com</SelectItem>
-                    <SelectItem value="m@support.com">m@support.com</SelectItem>
+                    {['2016', '2017', '2018'].map((item) => (
+                      <SelectItem key={item} value={`${item}-01-01`}>
+                        {item}
+                      </SelectItem>
+                    ))}
                   </SelectContent>
                 </Select>
               </FormControl>
               <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="County_of_Residence"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>County of residence</FormLabel>
+              <FormControl>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select a county" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    {['2016', '2017', '2018'].map((item) => (
+                      <SelectItem key={item} value={`${item}-01-01`}>
+                        {item}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="Abnormal_Conditions_Checked_YN"
+          render={({ field }) => (
+            <FormItem className="flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4">
+              <FormControl>
+                <Checkbox
+                  checked={field.value}
+                  onCheckedChange={field.onChange}
+                />
+              </FormControl>
+              <div className="space-y-1 leading-none">
+                <FormLabel className="cursor-pointer">
+                  Abnormal conditions
+                </FormLabel>
+              </div>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="Ave_Birth_Weight_gms"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Average weight of birth (GRAMS)</FormLabel>
+              <FormControl>
+                <Slider
+                  defaultValue={[field.value[0]]}
+                  max={3400}
+                  min={3000}
+                  onValueChange={field.onChange}
+                />
+              </FormControl>
+              <FormMessage />
+              <p>{field.value[0]}</p>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="Ave_Age_of_Mother"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Average age of mother (YEARS)</FormLabel>
+              <FormControl>
+                <Slider
+                  defaultValue={[field.value[0]]}
+                  onValueChange={field.onChange}
+                />
+              </FormControl>
+              <FormMessage />
+              <p>{field.value[0]}</p>
             </FormItem>
           )}
         />
--- front/src/services/queryBuilder.ts
+++ front/src/services/queryBuilder.ts
@@ -1,24 +1,63 @@
 import { BigQuery } from '@google-cloud/bigquery'
 
-export async function queryStackOverflow() {
-  // Queries a public Stack Overflow dataset.
+const bigquery = new BigQuery({
+  projectId: 'techplus-stage2',
+  keyFilename: '/home/brito/Dev/tech+-test2/front/bigquery-key.json'
+})
 
-  // Create a client
-  const bigquery = new BigQuery({
-    projectId: 'techplus-stage2',
-    keyFilename: '/home/brito/Dev/tech+-test2/front/bigquery-key.json'
-  })
+const DB_NAME = 'bigquery-public-data.sdoh_cdc_wonder_natality'
 
-  // The SQL query to run
-  const sqlQuery = `SELECT
-    CONCAT(
-      'https://stackoverflow.com/questions/',
-      CAST(id as STRING)) as url,
-    view_count
-    FROM \`bigquery-public-data.stackoverflow.posts_questions\`
-    WHERE tags like '%google-bigquery%'
-    ORDER BY view_count DESC
-    LIMIT 10`
+export enum TABLE_NAME {
+  abnormal_conditions = 'county_natality_by_abnormal_conditions',
+  default = 'county_natality'
+}
+
+interface whereProps {
+  anormal_Conditions_Checked_YN?: boolean
+  Ave_Age_of_Mother?: number
+  Ave_Birth_Weight_gms?: number
+  County_of_Residence: string
+  Mothers_Single_Race?: string
+  Year?: string
+}
+
+interface likeProps {
+  County_of_Residence: string
+  Mothers_Single_Race?: string
+}
+
+type selectProps = keyof whereProps | '*' | 'Births'
+
+interface QueryBuilderProps {
+  limit?: number
+  where?: whereProps
+  select?: selectProps[]
+  like?: likeProps
+  tableName?: TABLE_NAME
+  join?: {
+    tableName: TABLE_NAME
+    on: string
+  }
+}
+
+export async function fetchQuery({
+  limit = 10,
+  where,
+  select,
+  like,
+  tableName = TABLE_NAME.default,
+  join
+}: QueryBuilderProps) {
+  const whereQuery = parseWhere(where)
+  const likeQuery = parseLike(like)
+  const selectQuery = select ? select.join(', ') : '*'
+
+  const sqlQuery = `SELECT ${selectQuery}
+    FROM \`${DB_NAME}.${tableName}\`
+    ${join ? `JOIN ${DB_NAME}.${join.tableName} ON ${join.on}` : ''}
+    ${where ? `WHERE ${whereQuery}` : ''}
+    ${like ? `LIKE ${likeQuery}` : ''} 
+    LIMIT ${limit}`
 
   const options = {
     query: sqlQuery,
@@ -28,12 +67,28 @@ export async function queryStackOverflow() {
 
   // Run the query
   const [rows] = await bigquery.query(options)
-
+  console.log(rows)
   console.log('Query Results:')
   rows.forEach((row) => {
-    const url = row.url
-    const viewCount = row.view_count
-    console.log(`url: ${url}, ${viewCount} views`)
+    const county = row.County_of_Residence
+    console.log(`county: ${county}`, `rows: ${JSON.stringify(rows.length)}`)
   })
 }
 
+const parseWhere = (where?: whereProps) => {
+  if (!where) return ''
+  return Object.entries(where)
+    .map(([key, value]) => {
+      if (typeof value === 'string') return `${key} = '${value}'`
+      return `${key} = ${value}`
+    })
+    .join(' AND ')
+}
+const parseLike = (like?: likeProps) => {
+  if (!like) return ''
+  Object.entries(like)
+    .map(([key, value]) => {
+      return `${key} LIKE '%${value}%'`
+    })
+    .join(' AND ')
+}
